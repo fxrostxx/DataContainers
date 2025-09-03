6e94b3d6fc9145048fd61d2f8233157662b59967
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-09-03 21:42:42 +0300

    List overrun in progress

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -8,22 +8,28 @@ class Element
 private:
 	int Data;
 	Element* pNext;
+	static int count;
 
 public:
 	Element(int Data, Element* pNext = nullptr)
 	{
 		this->Data = Data;
 		this->pNext = pNext;
+		++count;
 
 		cout << "EConstructor: " << this << endl;
 	}
 	~Element()
 	{
+		--count;
+
 		cout << "EDestructor: " << this << endl;
 	}
 
 	friend class ForwardList;
 };
+
+int Element::count = 0;
 
 class ForwardList
 {
@@ -52,13 +58,9 @@ public:
 	}
 	void push_back(int Data)
 	{
-		Element* New = new Element(Data);
+		if (Head == nullptr) return push_front(Data);
 
-		if (Head == nullptr)
-		{
-			Head = New;
-			return;
-		}
+		Element* New = new Element(Data);
 
 		Element* Temp = Head;
 
@@ -73,13 +75,13 @@ public:
 		if (Head->pNext == nullptr)
 		{
 			delete Head;
-			Head = nullptr;
+			Head == nullptr;
 			return;
 		}
 
-		Element* Temp = Head;
+		Element* Erased = Head;
 		Head = Head->pNext;
-		delete Temp;
+		delete Erased;
 	}
 	void pop_back()
 	{
@@ -88,7 +90,7 @@ public:
 		if (Head->pNext == nullptr)
 		{
 			delete Head;
-			Head = nullptr;
+			Head == nullptr;
 			return;
 		}
 
@@ -101,26 +103,14 @@ public:
 	}
 	void insert(int Data, int index)
 	{
-		if (index == 1 || Head == nullptr)
-		{
-			push_front(Data);
-			return;
-		}
-		
+		if (Head == nullptr) return push_front(Data);
+
+		if (index >= Element::count) return push_back(Data);
+		if (index <= Element::count) return push_front(Data);
+
 		Element* Temp = Head;
-		int current_index{ 1 };
 
-		while (Temp && current_index < index - 1)
-		{
-			Temp = Temp->pNext;
-			++current_index;
-		}
-
-		if (Temp == nullptr)
-		{
-			cout << "Выход за границы списка" << endl;
-			return;
-		}
+		for (int i = 0; i < index - 1; ++i) Temp = Temp->pNext;
 
 		Element* New = new Element(Data);
 
@@ -131,30 +121,15 @@ public:
 	{
 		if (Head == nullptr) return;
 
-		if (index == 1)
-		{
-			pop_front();
-			return;
-		}
+		if (index == 1) return pop_front();
 
 		Element* Temp = Head;
-		int current_index{ 1 };
 
-		while (Temp && current_index < index - 1)
-		{
-			Temp = Temp->pNext;
-			++current_index;
-		}
+		for (int i = 0; i < index - 1; ++i) Temp = Temp->pNext;
 
-		if (Temp == nullptr || Temp->pNext == nullptr)
-		{
-			cout << "Выход за границы списка" << endl;
-			return;
-		}
-
-		Element* ToDelete = Temp->pNext;
-		Temp->pNext = ToDelete->pNext;
-		delete ToDelete;
+		Element* Erased = Temp->pNext;
+		Temp->pNext = Erased->pNext;
+		delete Erased;
 	}
 
 	void print() const
@@ -165,13 +140,18 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;
 		}
+
+		cout << "Количество элементов списка: " << Element::count << endl;
 	}
 };
+
+//#define BASE_CHECK
 
 int main()
 {
 	setlocale(LC_ALL, "");
 
+#ifdef BASE_CHECK
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 
@@ -179,22 +159,70 @@ int main()
 
 	for (int i = 0; i < n; ++i)
 	{
-		list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
+
+	list.print();
 
 	list.push_back(456);
 
 	list.push_front(123);
 
+	list.print();
+
 	list.pop_front();
 
 	list.pop_back();
 
-	list.insert(789, 3);
+	list.print();
 
-	list.erase(5);
+	int index;
+	int value;
+
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+	list.insert(value, index);
 
 	list.print();
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+
+	list.erase(index);
+
+	list.print();
+#endif // BASE_CHECK
+
+	ForwardList list1;
+
+	list1.push_back(0);
+	list1.push_back(1);
+	list1.push_back(1);
+	list1.push_back(2);
+
+	list1.print();
+
+	ForwardList list2;
+
+	list2.push_back(3);
+	list2.push_back(5);
+	list2.push_back(8);
+	list2.push_back(13);
+	list2.push_back(21);
+	list2.push_back(34);
+	list2.push_back(55);
+	list2.push_back(89);
+
+	list2.print();
+
+	int index;
+	int value;
+
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+	list1.insert(value, index);
+	list1.print();
 
 	return 0;
 }
