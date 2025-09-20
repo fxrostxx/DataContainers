a8d92ef0a205163005f5f9d7da3c68dd6b8439e4
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-09-20 17:44:59 +0300

    Factorial, Power, Fibonacci DONE

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -4,6 +4,9 @@ using std::cout;
 using std::endl;
 
 void elevator(int floor);
+unsigned long long int Factorial(int n);
+double Power(int n, int power);
+void Fibonacci(unsigned int n, unsigned int a = 0, unsigned int b = 1);
 
 int main()
 {
@@ -12,6 +15,22 @@ int main()
 	int n;
 	cout << "Введите номер этажа: "; cin >> n;
 	elevator(n);
+
+	cout << "Введите число: "; cin >> n;
+	cout << n << "! = " << Factorial(n) << endl;
+
+	int power;
+	cout << "Введите число: "; cin >> n;
+	cout << "Введите степень: "; cin >> power;
+	cout << n << "^" << power << " = " << Power(n, power) << endl;
+
+	unsigned int a;
+	while (true)
+	{
+		cout << "Введите число до которого вывести ряд Фибоначчи: "; cin >> a;
+		Fibonacci(a);
+		cout << endl;
+	}
 
 	return 0;
 }
@@ -26,4 +45,29 @@ void elevator(int floor)
 	cout << "Вы на " << floor << " этаже" << endl;
 	elevator(floor - 1);
 	cout << "Вы на " << floor << " этаже" << endl;
+}
+unsigned long long int Factorial(int n)
+{
+	if (n < 0) return 0;
+	if (n == 0 || n == 1) return 1;
+	return (unsigned long long int)n * Factorial(n - 1);
+}
+double Power(int n, int power)
+{
+	if (!power) return 1;
+	if (power < 0) return (double)1 / Power(n, -power);
+	return n * Power(n, power - 1);
+}
+void Fibonacci(unsigned int n, unsigned int a, unsigned int b)
+{
+	if (n == 0)
+	{
+		cout << 0;
+		return;
+	}
+	unsigned int c = a + b;
+	if (c > n) return;
+	if (c == 1) cout << a << ' ' << b << ' ';
+	cout << c << ' ';
+	Fibonacci(n, b, c);
 }
