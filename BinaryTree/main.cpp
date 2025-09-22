e97f1e666169d90aef95d39eaab5eb71c4b91434
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-09-22 23:20:54 +0300

    Sum, Avg, Clear DONE, Erase in progress

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -37,6 +37,7 @@ public:
 	}
 	~Tree()
 	{
+		clear(Root);
 		cout << "TDestructor: " << this << endl;
 	}
 	void insert(int Data, Element* Root)
@@ -54,16 +55,82 @@ public:
 			else insert(Data, Root->pRight);
 		}
 	}
+	void erase(int Data, Element* Root)
+	{
+		if (!this->Root || !Root) return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft && Root->pLeft->Data == Data)
+			{
+				if (!Root->pLeft->pLeft && !Root->pLeft->pRight)
+				{
+					delete Root->pLeft;
+					Root->pLeft = nullptr;
+				}
+				else if (Root->pLeft->pLeft && !Root->pLeft->pRight)
+				{
+					Root->pLeft = Root->pLeft->pLeft;
+					delete Root->pLeft;
+				}
+				else if (!Root->pLeft->pLeft && Root->pLeft->pRight)
+				{
+					Root->pRight = Root->pLeft->pRight;
+					delete Root->pLeft;
+				}
+				else if (Root->pLeft->pLeft && Root->pLeft->pRight)
+				{
+					//???
+				}
+			}
+			else erase(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight && Root->pRight->Data == Data)
+			{
+				if (!Root->pRight->pLeft && !Root->pRight->pRight)
+				{
+					delete Root->pRight;
+					Root->pRight = nullptr;
+				}
+				else if (Root->pRight->pLeft && !Root->pRight->pRight)
+				{
+					Root->pLeft = Root->pRight->pLeft;
+					delete Root->pRight;
+				}
+				else if (!Root->pRight->pLeft && Root->pRight->pRight)
+				{
+					Root->pRight = Root->pRight->pRight;
+					delete Root->pRight;
+				}
+				else if (Root->pRight->pLeft && Root->pRight->pRight)
+				{
+					//???
+				}
+			}
+			else erase(Data, Root->pRight);
+		}
+	}
+	void clear(Element* Root)
+	{
+		if (!Root) return;
+		clear(Root->pLeft);
+		clear(Root->pRight);
+		delete Root;
+		Root = nullptr;
+	}
 	int minValue(Element* Root)
 	{
-		/*if (!this->Root) return;
+		/*if (!Root) return INT_MIN;
+		if (!this->Root) return;
 		if (!Root->pLeft) return Root->Data;
 		else return minValue(Root->pLeft);*/
 		return !Root ? INT_MIN : !Root->pLeft ? Root->Data : minValue(Root->pLeft);
 	}
 	int maxValue(Element* Root)
 	{
-		/*if (!this->Root) return;
+		/*if (!Root) return INT_MAX;
+		if (!this->Root) return;
 		if (!Root->pRight) return Root->Data;
 		else return maxValue(Root->pRight);*/
 		return !Root ? INT_MAX : !Root->pRight ? Root->Data : maxValue(Root->pRight);
@@ -73,6 +140,18 @@ public:
 		/*if (!Root) return 0;
 		else return count(Root->pLeft) + count(Root->pRight) + 1;*/
 		return !Root ? 0 : count(Root->pLeft) + count(Root->pRight) + 1;
+	}
+	int Sum(Element* Root)
+	{
+		/*if (!Root) return 0;
+		else return Sum(Root->pLeft) + Sum(Root->pRight) + Root->Data;*/
+		return !Root ? 0 : Sum(Root->pLeft) + Sum(Root->pRight) + Root->Data;
+	}
+	double Avg(Element* Root)
+	{
+		/*if (!Root) return 0;
+		else return (double)Sum(Root) / count(Root);*/
+		return !Root ? 0 : (double)Sum(Root) / count(Root);
 	}
 	void print(Element* Root) const
 	{
@@ -97,7 +176,13 @@ int main()
 
 	cout << "Минимальное значение в дереве: " << tree.minValue(tree.get_Root()) << endl;
 	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.get_Root()) << endl;
-	cout << "Количество элементов в дереве: " << tree.count(tree.get_Root()) << endl;
+	cout << "Количество элементов дерева: " << tree.count(tree.get_Root()) << endl;
+	cout << "Сумма элементов дерева: " << tree.Sum(tree.get_Root()) << endl;
+	cout << "Среднее арифметическое элементов дерева: " << tree.Avg(tree.get_Root()) << endl;
+	cout << "Введите значение удаляемого элемента: "; cin >> n;
+	tree.erase(n, tree.get_Root());
+	tree.print(tree.get_Root());
+	cout << endl;
 
 	return 0;
 }
